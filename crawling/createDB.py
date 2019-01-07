19e8836c979b4177d82e85bf25d730ec6948a256
eric-yoo <ajoije729@gmail.com>
2019-01-07 09:23:40 +0900

    fix crawling exception

--- crawling/createDB.py
+++ crawling/createDB.py
@@ -11,8 +11,7 @@ import pandas as pd
 import openpyxl
 import csv
 import platform
-# ???? + ???? ?? + ???? ?? + ??(? ?? ? 100? ????, ????? 3??)
-# ???? 1: ???? ??? ?? ??/??/??
+
 
 if "Ubuntu" in platform.platform():
     driver = webdriver.Chrome('./chromedriver')
@@ -23,43 +22,43 @@ driver.get("https://www.earthtory.com/ko/area")
 time.sleep(1)
 
 country_list=[]
-# ???
+
 for i in range(1,21):
         country = driver.find_element_by_xpath("/html/body/div[7]/div/div[3]/a[%i]"%i).text
         a = re.findall(r'[a-zA-Z]', country)
         b = ''.join(a)
         country_list.append(b)
-        # print("asia",i)
-# ??
+
+
 for i in range(1,30):
         country = driver.find_element_by_xpath("/html/body/div[7]/div/div[5]/a[%i]"%i).text
         a = re.findall(r'[a-zA-Z]', country)
         b = ''.join(a)
         country_list.append(b)
-        # print("eu",i)
-# ????
+
+
 for i in range(1,8):
         country = driver.find_element_by_xpath("/html/body/div[7]/div/div[7]/a[%i]"%i).text
         a = re.findall(r'[a-zA-Z]', country)
         b = ''.join(a)
         country_list.append(b)
-        # print("oc",i)
-# ??
+
+
 for i in range(1,3):
         country = driver.find_element_by_xpath("/html/body/div[7]/div/div[9]/a[%i]"%i).text
         a = re.findall(r'[a-zA-Z]', country)
         b = ''.join(a)
         country_list.append(b)
-        # print("na",i)
-# ???
+
+
 for i in range(1,12):
         country = driver.find_element_by_xpath("/html/body/div[7]/div/div[11]/a[%i]"%i).text
         a = re.findall(r'[a-zA-Z]', country)
         b = ''.join(a)
         country_list.append(b)
-        # print("na",i)
 
-# df.to_csv("./countries.csv")
+
+
 url_all={}
 for enum,country in enumerate(country_list):
     for iter in range(1,11):
@@ -92,18 +91,6 @@ with open('landmark_db.csv', 'w', newline='') as csv_file:
             csv_writer.writerow(row)
             index+=1
 
-
-
-
-# 나라이름 + 관광명소 이름 + 관광명소 설명 + 사진(한 나라 당 100개 관광명소, 관광명소당 3개씩)
-# 해야될거 1: 관광명소 하나에 대한 이름/설명/사진
-# url_all={
-#     'Portugal':[
-#         ('Rossio','https://www.earthtory.com/ko/city/lisbon_10464/attraction/rossio_765550'),
-#         ('Largo Portas do Sol','https://www.earthtory.com/ko/city/lisbon_10464/attraction/largo-portas-do-sol_765407')
-#     ]
-# }
-
 try:
     os.mkdir("landmark")
 except:
@@ -111,36 +98,39 @@ except:
 with open('image_db.csv', 'w', newline='') as csv_file2:
     for country,landmark_list in url_all.items():
         for (landmark_name, landmark_url) in landmark_list:
-            driver.get(landmark_url)
-            time.sleep(1)
-
-
-
-            src = []
-            url1 = driver.find_element_by_xpath('/html/body/div[7]/div[1]/div[1]/div[1]/img')
-            src.append(url1.get_attribute('src'))
-            url2 = driver.find_element_by_xpath('/html/body/div[7]/div[1]/div[1]/div[2]/img')
-            src.append(url2.get_attribute('src'))
-            url3 = driver.find_element_by_xpath('/html/body/div[7]/div[1]/div[1]/div[3]/img')
-            src.append(url3.get_attribute('src'))
             try:
-                os.mkdir("landmark/{}".format(landmark_name))
-            except:
-                pass
+                driver.get(landmark_url)
+                time.sleep(1)
 
-            img_path = "landmark/{}/".format(landmark_name)
 
-            for i in range(0,3):
-                if src[i] == "https://www.earthtory.com/res/img/city/spot_info/spot_photo_add.gif":
-                    continue
-                # print(src[i],'\n')
+
+                src = []
+                url1 = driver.find_element_by_xpath('/html/body/div[7]/div[1]/div[1]/div[1]/img')
+                src.append(url1.get_attribute('src'))
+                url2 = driver.find_element_by_xpath('/html/body/div[7]/div[1]/div[1]/div[2]/img')
+                src.append(url2.get_attribute('src'))
+                url3 = driver.find_element_by_xpath('/html/body/div[7]/div[1]/div[1]/div[3]/img')
+                src.append(url3.get_attribute('src'))
                 try:
-                    urllib.request.urlretrieve(src[i], img_path+str(i+1)+".jpg")
-
-                    img_url='landmark/{}/{}'.format(landmark_name,i)
-                    row=[landmark_name, img_url]
-                    csv_writer = csv.writer(csv_file2)
-                    csv_writer.writerow(row)
-
+                    os.mkdir("landmark/{}".format(landmark_name))
                 except:
                     pass
+
+                img_path = "landmark/{}/".format(landmark_name)
+
+                for i in range(0,3):
+                    if src[i] == "https://www.earthtory.com/res/img/city/spot_info/spot_photo_add.gif":
+                        continue
+                
+                    try:
+                        urllib.request.urlretrieve(src[i], img_path+str(i+1)+".jpg")
+
+                        img_url='landmark/{}/{}'.format(landmark_name,i)
+                        row=[landmark_name, img_url]
+                        csv_writer = csv.writer(csv_file2)
+                        csv_writer.writerow(row)
+
+                    except:
+                        pass
+            except:
+                pass
