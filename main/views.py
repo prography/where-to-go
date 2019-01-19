5f710fa45da98b58c995d0a39e5697f9f433e5f7
injichoi <chldlswl1212@gmail.com>
2019-01-19 22:23:18 +0900

    connect selectpage

--- main/views.py
+++ main/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from main.models import Landmark, Image
 
 # Create your views here.
 
+
 def index_page(request):
     return render(request, 'main/index.html', {})
+
+
+def select_page(request):
+
+    return render(request, 'main/select.html', {})
