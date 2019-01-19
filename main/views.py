2edce414a20405fe9d682baf8676d5fb0ab5e57b
by2lucy <hjeongzz@naver.com>
2019-01-19 21:08:14 +0900

    view update 1

--- main/views.py
+++ main/views.py
@@ -18,7 +18,8 @@ def select_page(request):
         img3 = Image.objects.filter(cluster1=3).order_by('?').first()
         img4 = Image.objects.filter(cluster1=4).order_by('?').first()
 
-        return render(request, 'main/select.html', {'count': count + 1, 'images': [img1, img2, img3, img4]})
+        return render(request, 'main/select.html', {'count': count + 1,
+                                                    'images': [img1.url, img2.url, img3.url, img4.url]})
 
 
     elif count == 2:
@@ -30,7 +31,8 @@ def select_page(request):
         img3 = Image.objects.filter(cluster1=cluster1, cluster2=3).order_by('?').first()
         img4 = Image.objects.filter(cluster1=cluster1, cluster2=4).order_by('?').first()
 
-        return render(request, 'main/select.html', {'count': count + 1, 'images': [img1, img2, img3, img4]})
+        return render(request, 'main/select.html', {'count': count + 1,
+                                                    'images': [img1.url, img2.url, img3.url, img4.url]})
 
 
     elif count == 3:
@@ -43,8 +45,8 @@ def select_page(request):
         img3 = Image.objects.filter(cluster1=cluster1, cluster2=cluster2).order_by('?').first()
         img4 = Image.objects.filter(cluster1=cluster1, cluster2=cluster2).order_by('?').first()
 
-        return render(request, 'main/select.html', {'count': count + 1, 'images': [img1, img2, img3, img4]})
-
+        return render(request, 'main/select.html', {'count': count + 1,
+                                                    'images': [img1.url, img2.url, img3.url, img4.url]})
 
     # 결과
     elif count == 4:
@@ -52,6 +54,19 @@ def select_page(request):
         cluster1 = choice.cluster1
         cluster2 = choice.cluster2
         cluster3 = choice.cluster3
+
+        result = Image.objects.filter(cluster1=cluster1, cluster2=cluster2, cluster3=cluster3).order_by('?')
+        landmark = result.first().landmark
+        country = Landmark.objects.get(landmark=landmark).country
+
+        # resurl_img 개수만큼 append
+
+
+
+        si_img1 = result[1].url
+        si_img2 = result[2].url
+        si_img3 = result[3].url
+
 
         return render(request, 'main/resultPage.html', { })
 
