bcee8e29c4f9889ae62fa092773509eaef0d5555
kyeonglin song <kyeong_lin@naver.com>
2019-01-19 18:35:23 +0900

    modify views

--- main/models.py
+++ main/models.py
@@ -9,5 +9,7 @@ class Landmark(models.Model):
 class Image(models.Model):
     url = models.TextField()
     landmark = models.ForeignKey(Landmark, on_delete=models.CASCADE)
-
+    cluster1 = models.IntegerField(default=0)
+    cluster2 = models.IntegerField(default=0)
+    cluster3 = models.IntegerField(default=0)
 
--- main/views.py
+++ main/views.py
@@ -1,6 +1,35 @@
 from django.shortcuts import render
+from main.models import Landmark, Image
 
 # Create your views here.
 
 def index_page(request):
-    return render(request, 'templates/main/index.html', {})
+    return render(request, 'main/index.html', {})
+
+def select(request):
+    count = request.GET.get('count')
+    selected_img = request.GET.get('selected_img')
+
+    if count == 1:
+        img1 = Image.objects.filter(cluster1=0).order_by('?').first()
+        img2 = Image.objects.filter(cluster1=1).order_by('?').first()
+        img3 = Image.objects.filter(cluster1=2).order_by('?').first()
+        img4 = Image.objects.filter(cluster1=3).order_by('?').first()
+        return render(request, 'main/select.html', {'count': count + 1, 'image': [img1, img2, img3, img4]})
+
+    elif count == 2:
+        choice = Image.objects.filter(url=selected_img)
+        img1 = Image.objects.filter(cluster1=choice.cluster1).filter(cluster2=0).order_by('?').first()
+        img2 = Image.objects.filter(cluster1=choice.cluster1).filter(cluster2=1).order_by('?').first()
+        img3 = Image.objects.filter(cluster1=choice.cluster1).filter(cluster2=2).order_by('?').first()
+        img4 = Image.objects.filter(cluster1=choice.cluster1).filter(cluster2=3).order_by('?').first()
+        return render(request, 'main/select.html', {'count': count + 1, 'image': [img1, img2, img3, img4]})
+
+    elif count == 3:
+        choice = Image.objects.filter(url=selected_img)
+        img1 = Image.objects.filter(cluster2=choice.cluster2).filter(cluster3=0).order_by('?').first()
+        img2 = Image.objects.filter(cluster2=choice.cluster2).filter(cluster3=1).order_by('?').first()
+        img3 = Image.objects.filter(cluster2=choice.cluster2).filter(cluster3=2).order_by('?').first()
+        img4 = Image.objects.filter(cluster2=choice.cluster2).filter(cluster3=3).order_by('?').first()
+        return render(request, 'main/result.html', {})
+
