9c10da6d0c75dd2323908412b5b1070319b11059
by2lucy <hjeongzz@naver.com>
2019-01-19 22:02:14 +0900

    add app_name

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = "main"
+
 urlpatterns = [
     path('home/', views.index_page, name='index'),
     path('select/', views.select_page, name='select'),
